51de9549db2add3dda92d1a46f4fe2ca10804fa8
Dieter Plaetinck <dieter@raintank.io>
2015-05-04 18:29:36 -0400

    develop testing and core logic

--- pkg/alerting/alerting_test.go
+++ pkg/alerting/alerting_test.go
@@ -2,7 +2,9 @@ package alerting
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
+	"time"
 
 	"bosun.org/graphite"
 	. "github.com/smartystreets/goconvey/convey"
@@ -13,66 +15,96 @@ type fakeGraphite struct {
 }
 
 func (f *fakeGraphite) Query(r *graphite.Request) (graphite.Response, error) {
-	return nil, nil
+	return f.resp, nil
+}
+
+func NewFakeGraphiteWithTestValue(val int) *fakeGraphite {
+	fg := &fakeGraphite{}
+	fg.resp = graphite.Response{
+		graphite.Series{
+			Target: "test",
+			Datapoints: []graphite.DataPoint{
+				graphite.DataPoint{json.Number(fmt.Sprintf("%d", val)), json.Number("1234567890")},
+			},
+		},
+	}
+	return fg
 }
 
 func TestAlerting(t *testing.T) {
 
 	Convey("when evaluating graphite checks", t, func() {
-		Convey("Series median above threshold should trigger alert", func() {
-			checkDef := &CheckDef{
-				CritExpr: `median(graphite(test), "2m", "", "") < 100`,
+		Convey("Given a graphite check expression", func() {
+			checkDef := CheckDef{
+				CritExpr: `median(graphite("test", "2m", "", "")) > 100`,
 			}
 
-			fg := &fakeGraphite{}
-			fg.resp = graphite.Response{
-				graphite.Series{
-					Target: "test",
-					Datapoints: []graphite.DataPoint{
-						graphite.DataPoint{json.Number("150"), json.Number("1234567890")},
-					},
-				},
-			}
+			Convey("Series median above threshold should trigger alert", func() {
+				fg := NewFakeGraphiteWithTestValue(150)
+				evaluator, err := NewGraphiteCheckEvaluator(fg, checkDef)
+				So(err, ShouldBeNil)
 
-			evaluator := &GraphiteCheckEvaluator{
-				Context: fg,
-				Check:   checkDef,
-			}
-
-			Convey("Should eval threshold reached", func() {
-				res, err := evaluator.Eval()
+				res, err := evaluator.Eval(time.Now())
 				So(err, ShouldBeNil)
 				So(res, ShouldEqual, EvalResultCrit)
 			})
-		})
 
-		Convey("Series median above threshold should trigger alert", func() {
-			checkDef := &CheckDef{
-				CritExpr: `median(graphite(test), "2m", "", "") < 100`,
-			}
+			Convey("Series median equal to threshold should not trigger alert", func() {
+				fg := NewFakeGraphiteWithTestValue(100)
+				evaluator, err := NewGraphiteCheckEvaluator(fg, checkDef)
+				So(err, ShouldBeNil)
 
-			fg := &fakeGraphite{}
-			fg.resp = graphite.Response{
-				graphite.Series{
-					Target: "test",
-					Datapoints: []graphite.DataPoint{
-						graphite.DataPoint{json.Number("50"), json.Number("1234567890")},
-					},
-				},
-			}
+				res, err := evaluator.Eval(time.Now())
+				So(err, ShouldBeNil)
+				So(res, ShouldEqual, EvalResultOK)
+			})
 
-			evaluator := &GraphiteCheckEvaluator{
-				Context: fg,
-				Check:   checkDef,
-			}
+			Convey("Series median below threshold should not trigger alert", func() {
+				fg := NewFakeGraphiteWithTestValue(70)
+				evaluator, err := NewGraphiteCheckEvaluator(fg, checkDef)
+				So(err, ShouldBeNil)
 
-			Convey("Should eval threshold reached", func() {
-				res, err := evaluator.Eval()
+				res, err := evaluator.Eval(time.Now())
 				So(err, ShouldBeNil)
 				So(res, ShouldEqual, EvalResultOK)
 			})
 		})
 
-	})
+		Convey("Given two graphite check expressions", func() {
+			checkDef := CheckDef{
+				WarnExpr: `median(graphite("test", "2m", "", "")) < 150`,
+				CritExpr: `median(graphite("test", "2m", "", "")) < 100`,
+			}
 
+			Convey("Series median in critical region", func() {
+				fg := NewFakeGraphiteWithTestValue(99)
+				evaluator, err := NewGraphiteCheckEvaluator(fg, checkDef)
+				So(err, ShouldBeNil)
+
+				res, err := evaluator.Eval(time.Now())
+				So(err, ShouldBeNil)
+				So(res, ShouldEqual, EvalResultCrit)
+			})
+
+			Convey("Series median in warn region", func() {
+				fg := NewFakeGraphiteWithTestValue(100)
+				evaluator, err := NewGraphiteCheckEvaluator(fg, checkDef)
+				So(err, ShouldBeNil)
+
+				res, err := evaluator.Eval(time.Now())
+				So(err, ShouldBeNil)
+				So(res, ShouldEqual, EvalResultWarn)
+			})
+
+			Convey("Series median in OK region", func() {
+				fg := NewFakeGraphiteWithTestValue(150)
+				evaluator, err := NewGraphiteCheckEvaluator(fg, checkDef)
+				So(err, ShouldBeNil)
+
+				res, err := evaluator.Eval(time.Now())
+				So(err, ShouldBeNil)
+				So(res, ShouldEqual, EvalResultOK)
+			})
+		})
+	})
 }
--- pkg/alerting/models.go
+++ pkg/alerting/models.go
@@ -1,9 +1,11 @@
 package alerting
 
 import (
-	"fmt"
-
+	"bosun.org/cmd/bosun/cache"
+	"bosun.org/cmd/bosun/expr"
 	"bosun.org/graphite"
+	"fmt"
+	"time"
 )
 
 type CheckDef struct {
@@ -33,21 +35,13 @@ type Check struct {
 // 	return dsQuery.Result
 // }
 
-type CheckEvaluator interface {
-	Eval() (*CheckEvalResult, error)
-}
-
-type GraphiteCheckEvaluator struct {
-	Context graphite.Context
-	Check   *CheckDef
-}
-
 type CheckEvalResult int
 
 const (
 	EvalResultOK CheckEvalResult = 1 << iota
 	EvalResultWarn
 	EvalResultCrit
+	EvalResultUnknown
 )
 
 func (c CheckEvalResult) String() string {
@@ -58,44 +52,78 @@ func (c CheckEvalResult) String() string {
 		return "Warning"
 	case EvalResultCrit:
 		return "Critical"
+	case EvalResultUnknown:
+		return "Unknown"
 	default:
 		panic(fmt.Sprintf("Invalid CheckEvalResult value %d", int(c)))
 	}
 }
 
-type DefaultCheckEvaluator struct {
+type CheckEvaluator interface {
+	Eval() (*CheckEvalResult, error)
 }
 
-func (ce *GraphiteCheckEvaluator) Eval() (*CheckEvalResult, error) {
-	// 	exp, err := expr.New(job.expr, expr.Graphite)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	//
-	// 	// create cache
-	// 	// this is so that when bosun queries the same graphite query multiple times
-	// 	// like in (median(graphite("foo")> 10 || avg(graphite("foo") > 20)
-	// 	// it reuses the same resultsets internally.
-	// 	// cache is unbounded so that we are guaranteed internally consistent results
-	// 	// TODO recreate new cache at each second because cache is pointless at the next interval
-	// 	cacheObj := cache.New(0)
-	//
-	// 	// TODO once auth works, do it without rh. should work too because RH should only be used for bosun's own alerts and notifications
-	// 	rh := &sched.RunHistory{
-	// 		Cache:           cacheObj,
-	// 		Start:           job.ts, // this sets an explicit "until" to match the data this alert run is meant for, even when we are delayed
-	// 		Events:          make(map[expr.AlertKey]*sched.Event),
-	// 		Context:         nil,
-	// 		GraphiteContext: ce.Context,
-	// 		Logstash:        make([]string, 0),
-	// 	}
-	// 	results, _, err := exp.Execute(rh.Context, rh.GraphiteContext, rh.Logstash, rh.Cache, nil, rh.Start, 0, true, nil, nil, nil)
-	// 	fmt.Println(job.ts, job.expr)
-	// 	for _, res := range results.Results {
-	// 		fmt.Println(res.Group, res.Value)
-	// 	}
-	// 	spew.Dump(err)
-	// }
+type GraphiteCheckEvaluator struct {
+	Context  graphite.Context
+	Check    CheckDef
+	critExpr *expr.Expr
+	warnExpr *expr.Expr
+}
 
-	return nil, nil
+func NewGraphiteCheckEvaluator(c graphite.Context, check CheckDef) (*GraphiteCheckEvaluator, error) {
+	var warnExpr *expr.Expr
+	var critExpr *expr.Expr
+	var err error
+	if check.WarnExpr != "" {
+		warnExpr, err = expr.New(check.WarnExpr, expr.Graphite)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if check.CritExpr != "" {
+		critExpr, err = expr.New(check.CritExpr, expr.Graphite)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &GraphiteCheckEvaluator{
+		Context:  c,
+		Check:    check,
+		warnExpr: warnExpr,
+		critExpr: critExpr,
+	}, nil
+}
+
+func (ce *GraphiteCheckEvaluator) Eval(ts time.Time) (CheckEvalResult, error) {
+	// create cache
+	// this is so that when bosun queries the same graphite query multiple times
+	// like in (median(graphite("foo", "2m", "",""))> 10 || avg(graphite("foo", "2m", "","")) > 20)
+	// it reuses the same resultsets internally.
+	// cache is unbounded so that we are guaranteed consistent results
+	cacheObj := cache.New(0)
+
+	if ce.critExpr != nil {
+		results, _, err := ce.critExpr.Execute(nil, ce.Context, nil, cacheObj, nil, ts, 0, true, nil, nil, nil)
+		if err != nil {
+			return EvalResultUnknown, err
+		}
+		for _, res := range results.Results {
+			if int(res.Value.Value().(expr.Number)) > 0 {
+				return EvalResultCrit, nil
+			}
+		}
+	}
+	if ce.warnExpr != nil {
+		results, _, err := ce.warnExpr.Execute(nil, ce.Context, nil, cacheObj, nil, ts, 0, true, nil, nil, nil)
+		if err != nil {
+			return EvalResultUnknown, err
+		}
+		for _, res := range results.Results {
+			if int(res.Value.Value().(expr.Number)) > 0 {
+				return EvalResultWarn, nil
+			}
+		}
+	}
+
+	return EvalResultOK, nil
 }
